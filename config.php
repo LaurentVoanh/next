234f78a9058b9c2c8839a9c28522cd79dd1ed9e2
LaurentVoanh <121799615+LaurentVoanh@users.noreply.github.com>
2026-04-07 11:04:57 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,3 @@
+c'est la page php qui lors de l'installation regarde la configuration du serveur, toutes ses fonctions actives et les fonctions inactive, les limit etc, toute les info
+elle met cela dans la bdd sqlite qui s'apelle "server" et elle ne le fait qu'une fois
+c'est là ou on met les config du site importante
