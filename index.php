e8f9507c212a62a6339750285574d29db43f8b66
LaurentVoanh <121799615+LaurentVoanh@users.noreply.github.com>
2026-04-07 11:10:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-c'est le portail public
+c'est le portail public, fond tres clair couleur  aleatoire, texte noir
+Le site s'appelle : BOOM FORTUNE
+Slogant : start up gratuite avec l'ia
+le menu
+un formulaire : quel est l'idée de votre strat up
+le bouton : "creer avec l'ia"
+en dessous: c'est gratuit, rapide, immédiat, simple et illimité
+et dans un bloc en dessous : la liste des start up deja créé dans la bdd sqlite STARTUP
